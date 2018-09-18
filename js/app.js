ef7473d4b93da9a4413037063c72c97954cf9107
vadrsa <davitasryan3@gmail.com>
2018-09-18 17:57:05 +0400

    loader

--- js/app.js
+++ js/app.js
@@ -1,8 +1,22 @@
 $(window).on("scroll", function() {
-  if($(window).scrollTop() > 50) {
-      $(".topBar").addClass("active");
-  } else {
-      //remove the background property so it comes transparent again (defined in your css)
-     $(".topBar").removeClass("active");
-  }
+    if($(window).scrollTop() > 50) {
+        $(".topBar").addClass("active");
+    } else {
+        //remove the background property so it comes transparent again (defined in your css)
+        $(".topBar").removeClass("active");
+    }
 });
+const loadingInterval = setInterval(function(){
+    let percent = parseInt($('.percentage').html().replace('%', ''));
+    if(percent == 100) {
+        endLoading();
+        return;
+    }
+    $('.percentage').html(++percent + '%');
+}, 10);
+
+function endLoading(){
+    clearInterval(loadingInterval);
+    $('.pageWrapper').show();
+    $('.loadingScreen').fadeOut();
+}
