4302c174e5fbe23961ba5c08443e9d34dedd669a
vadrsa <davitasryan3@gmail.com>
2018-09-18 18:07:16 +0400

    loader

--- js/app.js
+++ js/app.js
@@ -12,8 +12,10 @@ const loadingInterval = setInterval(function(){
         endLoading();
         return;
     }
-    $('.percentage').html(++percent + '%');
-}, 10);
+    const add =  Math.floor(Math.random()*10+1);
+    const res = percent+add;
+    $('.percentage').html(res > 100? 100: res + '%');
+}, 60);
 
 function endLoading(){
     clearInterval(loadingInterval);
